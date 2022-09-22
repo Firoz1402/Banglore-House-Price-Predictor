eb9d78ef2df50f61fcd3e739a24f331bdd6e752d
Firoz1402 <firozanjum537@gmail.com>
2022-09-22 17:35:16 +0530

    server fixed

--- static/app.js
+++ static/app.js
@@ -27,7 +27,7 @@ function getBathValue() {
     var estPrice = document.getElementById("uiEstimatedPrice");
   
     // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-    var url = "http://127.0.0.1:5000/predict_house_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    var url = "/predict_house_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
   
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
@@ -44,7 +44,7 @@ function getBathValue() {
   function onPageLoad() {
     console.log( "document loaded" );
     // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-    var url = "http://127.0.0.1:5000/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    var url = "/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
         if(data) {
